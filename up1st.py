1b6d3c53b10cfdbe7fef1b4b11402c6d2824a0b3
Yang Cai <yc631207248@live.com>
2016-03-12 18:21:01 +0800

    Thought I've done.........

--- up1st.py
+++ up1st.py
@@ -1,3 +1,8 @@
 #This program is for corrcting the user's input by making the fisrt letter upwrote.
 def up1(name):
     return name[0].upper()+name[1:].lower()
+
+#Test the up1
+c=map(up1,['jaSon','dicK','JohNson'])
+for x in c:
+    print x
